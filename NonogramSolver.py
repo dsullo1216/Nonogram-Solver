dd3d98e323e1a2ec682a2afcd15d108d278ceb23
David Sullo <dsullo@bu.edu>
2021-05-11 15:27:18 -0400

    Added function to print 2D array as a square in console

--- NonogramSolver.py
+++ NonogramSolver.py
@@ -9,7 +9,7 @@ def main():
     puzzle_grid = [[None for i in range(grid_dim)] for j in range(grid_dim)] # Initializes the 2D Array which will contain the grid itself
     valid_rows = [[3,1], [1,1,1], [5], [2], [4]] # Array containing the correct number of "filled-in" squares for the rows of the grid
     valid_cols = [[3], [1,3], [5], [1,1], [3,1]] # Array containing the correct number of "filled-in" squares for the columns of the grid
-    
+    print_grid(puzzle_grid)
 
     return 0
 
@@ -28,5 +28,24 @@ def nonogram_solver(puzzle_grid, valid_rows, valid_cols):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+print_grid(): Simple function that takes 2D array as an input and prints each row one by one to present a square grid in the console
+"""
+def print_grid(grid):
+    for r in range(len(grid)):
+        print(grid[r])
 
 main()
