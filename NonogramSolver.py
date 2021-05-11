2d67343c2c76947fc0204c3974e08085a544194e
David Sullo <dsullo@bu.edu>
2021-05-10 22:43:26 -0400

    Added code to initialize the 2D list for puzzle
    Adding arrays for the valid number of squares per row/col

---
+++ NonogramSolver.py
@@ -0,0 +1,15 @@
+
+def main():
+    grid_dim = 5 
+    puzzle_grid = [[None for i in range(grid_dim)] for j in range(grid_dim)] # Initializes the 2D Array which will contain the grid itself
+    valid_rows = [[3,1], [1,1,1], [5], [2], [4]] # Array containing the correct number of "filled-in" squares for the rows of the grid
+    valid_cols = [[3], [1,3], [5], [1,1], [3,1]] # Array containing the correct number of "filled-in" squares for the columns of the grid
+    return 0
+
+
+
+
+
+
+
+main()
